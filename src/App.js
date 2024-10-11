d9cf23bbc2fb24fe6a8915a51f9670682adcd537
Sam Lubin <samlubin@yahoo.com>
2024-10-11 14:21:25 -0400

    built chron page, plus some styles and better navigation logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import Page  from './Page';
 import { slugifyDirector } from './Helpers'
 import './App.css';
-
-
 
 function App() {
 	const location = useLocation();
@@ -17,39 +15,61 @@ function App() {
 			const currentSlug = location.pathname.split('/')[1]
 			// an empty slug should be the homepage:
 			setSlug(currentSlug === '' ? 'the-american-cinema' : currentSlug)
+			// if there is a directorId, scroll to the relevant director blurb:
+			const directorId = location.hash.split('#')[1]
+			const directorEl = document.getElementById(directorId);
+			if(directorEl){
+				directorEl.scrollIntoView()
+			}
 		});
-
 	}, [location]);
 
-	const handleNavClick = (slug, e) => {
-		// prevent page load:
+	const handleNavClick = (slug, directorId, e) => {
+		// prevent page load so we only ever load data on request rather than on navigation:
 		e.preventDefault();
 		// still have the url update:
 		window.history.replaceState(null, "New Page Title", slug);
-		// setting slug effectively navigates by showing the relevant content:
+		// setting slug effectively 'navigates' by showing the relevant content:
 		setSlug(slug);
-		// make sure we return to the top of the page when we navigate:
-		window.scrollTo(0, 0);
+		const directorEl = document.getElementById(directorId);
+		if(directorEl){
+			// if there is a directorId, scroll to the corresponding director blurb
+			directorEl.scrollIntoView({ behavior: 'smooth' })
+		} else {
+			// make sure we return to the top of the page when we 'navigate' to a new page:
+			window.scrollTo(0, 0);
+		}	
+	}
+
+	const directorLink = (d, di, chapterSlug) => {
+		const directorId = slugifyDirector(d.name);
+		const directorName = d.name.split('(')[0].trim();
+		const slug = chapterSlug + '#' + directorId
+		return(
+			<li key={'d_' + di}>
+				<a onClick={(e) => handleNavClick(slug, directorId, e)} href={'#' + directorName}>{directorName}</a>
+			</li>
+		)
 	}
 
 
 	return (
 		<div className="App">
 
-			<div className="main-container" style={{padding: '10px'}}>
-				<div>
-					{pages.filter((p) => p['slug'] === slug).map((p,i) => (
+			<div className="main-container">
+				<div style={{paddingLeft: '20px'}}>
+					{pages.filter((p) => p['slug'] === slug.split('#')[0]).map((p,i) => (
 						<Page key={i} data={p}></Page> 
 					))}	
 				</div>
-				<div style={{padding: '10px'}}>
+				<div style={{paddingLeft: '20px'}}>
 					<nav style={{position: 'sticky', top: '0', height: '100vh', overflow: 'scroll'}}>
 						{pages.map((p, pi) => (
 							<div key={'p_' + pi} style={{paddingBottom:'10px'}}>
 								<li className="list-style-none">
-									<a href='#' onClick={(e) => handleNavClick(p.slug, e)}>{ p.chapter_roman_num ? p.chapter_roman_num + '. ' + p.title : p.title }</a>
+									<a href='/' onClick={(e) => handleNavClick(p.slug, null, e)}>{ p.chapter_roman_num ? p.chapter_roman_num + '. ' + p.title : p.title }</a>
 								</li>
-								{ p.chapter_num && p.slug === slug ? p.directors.map((d, di) => (<li key={'d_' + di}><a href={'#' + slugifyDirector(d.name)}>{d.name.split('(')[0].trim()}</a></li>)) : null }
+								{ p.chapter_num && p.slug === slug ? p.directors.map((d, di) => (directorLink(d, di, p.slug))) : null }
 							</div>
 						))}
 					</nav>
