32f711250649d8b07ca45c2c397aeb68ede0a17f
Sam Lubin <samlubin@yahoo.com>
2024-10-18 12:06:14 -0400

    enforce arial font, plus some light styles

--- src/Chapter.js
+++ src/Chapter.js
@@ -5,10 +5,10 @@ const Chapter = forwardRef((props, ref) => {
 	
 	return (
 		<div>
-			<h1>{props.data.chapter_roman_num}. {props.data.title}</h1>
-			<em>{props.data.blurb}</em>
+			<h1 style={{fontFamily: 'arial'}}>{props.data.chapter_roman_num}. {props.data.title}</h1>
+			<em style={{fontFamily: 'georgia'}}>{props.data.blurb}</em>
 
-			<div>
+			<div style={{fontFamily: 'arial'}}>
 				{props.data.directors.map((d,di) => (
 					<div key={di}>
 						<h2 id={slugifyDirector(d.name)}>{d.name}</h2>
